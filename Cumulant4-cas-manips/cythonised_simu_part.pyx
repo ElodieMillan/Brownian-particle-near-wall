1ba3df5a0d205940d48862fd213ecb60ec6290de
e.millan <elodyy.millan@gmail.com>
2022-09-16 17:54:04 +0200

    MAJ algo MersenneTwister pour la seed!

--- Cumulant4-cas-manips/cythonised_simu_part.pyx
+++ Cumulant4-cas-manips/cythonised_simu_part.pyx
@@ -4,7 +4,9 @@ import numpy as np
 cimport numpy as np
 cimport cython
 from libc.math cimport exp, sqrt, log, cosh, sinh
-from libc.stdlib cimport rand, RAND_MAX
+from libc.stdlib cimport rand, RAND_MAX, srand
+import time
+
 
 cdef double pi = np.pi
 
@@ -174,24 +176,103 @@ cdef double positionZi_cython(double zi_1, double rng, double dt, double a,
 
     return zi
 
+## Random uniform classic
 @cython.nonecheck(False)
 @cython.cdivision(True)
 cdef double random_uniform():
     cdef double r = rand()
     return r / RAND_MAX
 
+## MERSENNE TWISTER ALGORITHM to generate a random number
+cdef unsigned NN = 312
+cdef unsigned MM = 156
+cdef unsigned long long MATRIX_A = 0xB5026F5AA96619E9ULL
+cdef unsigned long long UM = 0xFFFFFFFF80000000ULL
+cdef unsigned long long LM = 0x7FFFFFFFULL
+cdef unsigned long long mt[312]
+cdef unsigned mti = NN + 1
+cdef unsigned long long mag01[2]
+
+@cython.nonecheck(False)
+cdef mt_seed(unsigned long long seed):
+    global mt
+    global mti
+    global mag01
+    global NN
+    global MATRIX_A
+    mt[0] = seed
+    for mti in range(1,NN):
+        mt[mti] = (6364136223846793005ULL * (mt[mti-1] ^ (mt[mti-1] >> 62)) + mti)
+    mag01[0] = 0ULL
+    mag01[1] = MATRIX_A
+    mti = NN
+
+@cython.nonecheck(False)
+cdef unsigned long long genrand64():
+    cdef int i
+    cdef unsigned long long x
+    global mag01
+    global mti
+    global mt
+    global NN
+    global MM
+    global UM
+    global LM
+    if mti >= NN:
+        for i in range(NN-MM):
+            x = (mt[i]&UM) | (mt[i+1]&LM)
+            mt[i] = mt[i+MM] ^ (x>>1) ^ mag01[int(x&1ULL)]
+        for i in range(NN-MM, NN-1):
+            x = (mt[i]&UM)|(mt[i+1]&LM)
+            mt[i] = mt[i+(MM-NN)] ^ (x>>1) ^ mag01[int(x&1ULL)]
+        x = (mt[NN-1]&UM)|(mt[0]&LM)
+        mt[NN-1] = mt[MM-1] ^ (x>>1) ^ mag01[int(x&1ULL)]
+        mti = 0
+    x = mt[mti]
+    mti += 1
+    x ^= (x >> 29) & 0x5555555555555555ULL
+    x ^= (x << 17) & 0x71D67FFFEDA60000ULL
+    x ^= (x << 37) & 0xFFF7EEE000000000ULL
+    x ^= (x >> 43);
+    return x
+
+# Seed the random number generator
+@cython.nonecheck(False)
+cdef seed_random(unsigned long long seed):
+    """
+    Seed the C random number generator with the current system time.
+    :return: none
+    """
+    if seed == 0:
+        mt_seed(time.time())
+    else:
+        mt_seed(seed)
+
+#BOX-MULLER ALGORITHM : méthode polaire pour éviter les calculs de cos et sin
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef double random_gaussian(): #Box-muller algorithm : méthode polaire pour éviter les calculs de cos et sin
+cdef double uniform_rv():
+    """
+    Generate a uniform random variable in [0,1]
+    :return: (double) a random uniform number in [0,1]
+    """
+    return (genrand64() >> 11) * (1.0/9007199254740991.0)
+
+
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef double random_gaussian():
     cdef double x1, x2, w
 
     w = 2.0
     while (w >= 1.0):
-        x1 = 2.0 * random_uniform() - 1.0
-        x2 = 2.0 * random_uniform() - 1.0
+        x1 = 2.0 * uniform_rv() - 1.0
+        x2 = 2.0 * uniform_rv() - 1.0
         w = x1 * x1 + x2 * x2
     w = ((-2.0 * log(w)) / w) ** 0.5
     return x1 * w
+
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -211,6 +292,7 @@ cpdef double[:,:] trajectory_cython(unsigned long int Nt,
     cdef double x = res[0,0]
     cdef double y = res[1,0]
     cdef double z = res[2,0]
+    seed_random(0)
 
     for i in range(1, Nt):
         for j in range(0, Nt_sub):
